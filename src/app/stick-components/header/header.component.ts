234e921b4f8c1394d7c178d5466294ea8e77d3f4
Erwan JOURDAN <erwan.jourdan@outlook.fr>
2021-11-25 12:57:24 +0100

    ajout reaction nom du projet dans la navbar

--- src/app/stick-components/header/header.component.ts
+++ src/app/stick-components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/shared/services/global.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private globalService : GlobalService) { }
+
+  isLoggedIn() : boolean {
+    return this.globalService.isLoggedIn();
+  }
 
   ngOnInit(): void {
   }

6499e7d3d72cebb6099015b5cd90afb4360d145f
Doriane Narbeburu <narbe.do@gmail.com>
2021-11-25 14:32:42 +0100

    Ajout package-lock.json et app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,15 @@ import { FormEditorComponent } from './form-edition/form-editor/form-editor.comp
 import { ProfilDetailsComponent } from './profil-details/profil-details.component';
 import { ProfilDetailsEditorComponent } from './profil-details-editor/profil-details-editor.component';
 import { FormVideoComponent } from './form-collaborator/form-video/form-video.component';
+import { FormCollaboratorLatestMissionComponent } from './form-collaborator/form-collaborator-latest-mission/form-collaborator-latest-mission.component';
+import { FormCollaboratorNextMissionComponent } from './form-collaborator/form-collaborator-next-mission/form-collaborator-next-mission.component';
 
 const routes: Routes = [
   {path: "", redirectTo: 'home', pathMatch: 'full'},
   {path: "home", component:HomeComponent},
   {path: "form", component: FormCollaboratorSituationComponent},
+  {path: "form/latest", component: FormCollaboratorLatestMissionComponent},
+  {path: "form/next", component: FormCollaboratorNextMissionComponent},
   {path: "form-editor", component: FormEditorComponent},
   {path: "list-admin", component: ListAdminComponent},
   {path: "profil-details", component: ProfilDetailsComponent},

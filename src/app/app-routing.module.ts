c45cad3bdd77b1adde9b29761eea66d48b9e1e9f
Valentin Ancel <valentin-ancel@hotmail.fr>
2021-11-29 17:25:24 +0100

    je recup pour regler probleme list-admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
   
 import { FormCollaboratorSituationComponent } from './form-collaborator/form-collaborator-situation/form-collaborator-situation.component';
 import { ListAdminComponent } from './administration/list-admin/list-admin.component';
+import { FormAddAdminComponent } from './administration/form-add-admin/form-add-admin.component';
 import { ItemAdminComponent } from './administration/item-admin/item-admin.component';
 import { HomeComponent } from './home/home.component';
 import { FormEditorComponent } from './form-edition/form-editor/form-editor.component';
@@ -21,7 +22,8 @@ const routes: Routes = [
   {path: "form/next", component: FormCollaboratorNextMissionComponent},
   {path: "form", component: FormCollaboratorSituationComponent},
   {path: "form-editor", component: FormEditorComponent, canActivate:[AdminGuard]},
-  {path: "list-admin", component: ListAdminComponent, canActivate:[AdminGuard]},
+  {path: "list-admin", component: ListAdminComponent},
+  {path: "add-user", component: FormAddAdminComponent},
   {path: "profil-details", component: ProfilDetailsComponent, canActivate:[AccessGuardGuard]},
   {path: "profil-details-editor", component: ProfilDetailsEditorComponent, canActivate:[AdminGuard]},
   {path: "upload-video", component: FormVideoComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -67,7 +66,6 @@ import { AdminGuard } from './shared/services/admin.guard';
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatDialogModule, //Pour pouvoir faire un pop-up 
     HttpClientModule
   ],
   providers: [GlobalService, AccessGuardGuard, AdminGuard],
